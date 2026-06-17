b10fe2a5b50195e9eb2cf50629d36325157a812a
Mohammad_Jub@id786 <mohammadjubaid76@gmail.com>
2026-06-17 11:42:55 +0530

    Add files via upload

---
+++ 1_code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num;
+    for(int num=1; num<=5;num++) {
+        cout<<num<<"";
+
+    }
+    cout<<endl;
+    cout<<"Last value of num= "<<num<<endl;
+    return 0;
+}
---
+++ 1_to_n.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout<<"Enter your number";
+    cin>>n;
+    for(int i;i<=n;i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
